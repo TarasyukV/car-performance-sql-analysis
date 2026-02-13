b3f46428ebf14be804fdb9f7365d818fbfa5ee2a
Volodymyr <volodymyr.tarasyuk97@gmail.com>
2026-02-13 18:38:06 +0200

    Update project_queries.sql

--- project_queries.sql
+++ project_queries.sql
@@ -27,3 +27,36 @@ GROUP BY car_make
 HAVING COUNT(*) >= 3
 ORDER BY performance_index DESC
 LIMIT 10;
+
+
+
+
+/* ====================================================
+Business Question 1:
+How does horsepower influence vehicle pricing 
+across different performance segments?
+====================================================
+
+Logic:
+- Group horsepower into 100 HP intervals
+- Calculate average price per group
+- Exclude electric vehicles
+- Exclude null values
+- Keep only groups with sufficient observations
+==================================================== */
+
+SELECT 
+    CONCAT(
+        FLOOR(horsepower/100)*100, 
+        '-', 
+        FLOOR(horsepower/100)*100 + 99
+    ) AS hp_group,
+    ROUND(AVG(price_in_usd), 0) AS avg_price,
+	COUNT(*) AS car_count
+FROM cars
+WHERE horsepower IS NOT NULL
+  AND price_in_usd IS NOT NULL
+  AND engine_type <> 'Electric'
+GROUP BY FLOOR(horsepower/100)
+Having COUNT(*) > 5
+ORDER BY FLOOR(horsepower/100);
