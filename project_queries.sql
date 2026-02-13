f8631d1889c959f94e02d5b4f169d4dd63c1c275
Volodymyr <volodymyr.tarasyuk97@gmail.com>
2026-02-13 11:27:38 +0200

    project_queries.sql

---
+++ project_queries.sql
@@ -0,0 +1,29 @@
+/*
+============================================================
+Project: car-performance-sql-analysis
+Author: Volodymyr Tarasyuk
+Description:
+This file contains all SQL queries used to answer
+the business questions described in the README file.
+============================================================
+*/
+/* ============================================================
+Business Question 1:
+Performance Efficiency Ranking
+============================================================ */
+SELECT 
+    car_make,
+    ROUND(AVG(horsepower),2) AS avg_hp,
+    ROUND(AVG(zero_to_sixty_time),2) AS avg_speed,
+    ROUND(
+        AVG(horsepower) / AVG(zero_to_sixty_time),
+    2) AS performance_index,
+    COUNT(*) AS model_count
+FROM cars
+WHERE engine_type <> 'Electric'
+  AND horsepower IS NOT NULL
+  AND zero_to_sixty_time IS NOT NULL
+GROUP BY car_make
+HAVING COUNT(*) >= 3
+ORDER BY performance_index DESC
+LIMIT 10;
